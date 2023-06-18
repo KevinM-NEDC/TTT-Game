82e00533e1ab480dafc2c64137bd3fbc049adfd4
Kevin Mach <kmach3@uci.edu>
2023-06-17 22:12:26 -0700

    Create empty Player class

---
+++ game/player.py
@@ -0,0 +1,7 @@
+class Player:
+    """
+    A class to represent a player in the game.
+
+    Players will relate to the game status when performing actions,
+    and have no meaning outside game status objects.
+    """
